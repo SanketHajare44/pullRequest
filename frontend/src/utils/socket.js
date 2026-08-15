472c1e89feeffb124877156dc14e360027fe9a2f
SanketHajare44 <sankethajare44@gmail.com>
2026-08-15 14:33:37 +0530

    Fix production Socket.IO connection

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,13 +1,9 @@
+
 import io from "socket.io-client";
 import { BASE_URL } from "./constant";
 
 export const createSocketConnection = () => {
-    if (location.hostname === "localhost") {
-        return io(BASE_URL);
-    }
-
-    return io("/", {
-        path: "/api/socket.io",
+    return io(BASE_URL, {
         withCredentials: true,
     });
 };
